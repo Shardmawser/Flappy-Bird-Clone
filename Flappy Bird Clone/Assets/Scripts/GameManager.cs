e80e7e54f7557c60f90801c1e9c8aed422f10577
Shardmawser <abantolin777@gmail.com>
2023-01-05 12:56:29 +0800

    Ground resets when leaving bounds

--- Flappy Bird Clone/Assets/Scripts/GameManager.cs
+++ Flappy Bird Clone/Assets/Scripts/GameManager.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    Vector2 boundaries;
+    Vector2 screenBounds;
+    public GameObject ground;
+    public Transform resetPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ground.transform.position.x < screenBounds.x * -1)
+        {
+            ground.transform.position = resetPos.position;
+        }
     }
 }
