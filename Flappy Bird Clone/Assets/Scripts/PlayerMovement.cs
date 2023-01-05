96fe843175cbcf06de8a68d1d8d134f1266a8bf7
Shardmawser <abantolin777@gmail.com>
2023-01-05 11:50:40 +0800

    Player can now thrust up into the air

--- Flappy Bird Clone/Assets/Scripts/PlayerMovement.cs
+++ Flappy Bird Clone/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float jumpForce = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MyInput();
+    }
+
+    void MyInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
