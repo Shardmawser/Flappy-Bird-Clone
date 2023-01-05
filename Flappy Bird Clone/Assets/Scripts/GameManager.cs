98abfa21014fc360141212619d9bdbfb9b34b572
Shardmawser <abantolin777@gmail.com>
2023-01-05 13:02:28 +0800

    Moved functionality to Ground.cs

--- Flappy Bird Clone/Assets/Scripts/GameManager.cs
+++ Flappy Bird Clone/Assets/Scripts/GameManager.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    Vector2 screenBounds;
-    public GameObject ground;
-    public Transform resetPos;
-    public float offset;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)); 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(ground.transform.position.x + offset < screenBounds.x * -1)
-        {
-            ground.transform.position = resetPos.position;
-        }
-    }
+    
 }
---
+++ Flappy Bird Clone/Assets/Scripts/Ground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    Vector2 screenBounds;
+    public Transform resetPos;
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x + offset < screenBounds.x * -1)
+        {
+            transform.position = resetPos.position;
+        }
+    }
+}
